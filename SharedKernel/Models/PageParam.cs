a5a7f119c58b580f24635a321907522857519bb8
Gregory Fox <gregory.fox@tapiit.com>
2022-06-10 11:32:58 +0100

    Added PageParam class to make data pagination request model, that will be used later between repository and application services layer.

---
+++ SharedKernel/Models/PageParam.cs
@@ -0,0 +1,24 @@
+﻿namespace SharedKernel.Models
+{
+    public class PageParam
+    {
+        const int maxPageSize = 50;
+
+        private int _pageSize = 10;
+
+        public int PageSize 
+        { 
+            get
+            {
+                return _pageSize;
+            } 
+            
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+
+        public int PageNumber { get; set; } = 1;
+    }
+}
